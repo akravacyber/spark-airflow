272e08947353fd325fa9e65efd4e4c06ce5ed060
Arkadii Kravchuk <64816141+akravacyber@users.noreply.github.com>
2021-08-31 02:03:13 +0300

    Update example_bash_operator_classic.py

--- example_dags/example_bash_operator_classic.py
+++ example_dags/example_bash_operator_classic.py
@@ -35,7 +35,7 @@ dag = DAG(
     default_args=args,
     schedule_interval='0 0 * * *',
     dagrun_timeout=timedelta(minutes=60),
-    tags=['example']
+    tags=['example', 'test-branch', 'counter-0']
 )
 
 run_this_last = DummyOperator(
