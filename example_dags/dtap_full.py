09ec3d2742a2073ca2efaf7b120c6569c786c6f0
Arkadii Kravchuk <64816141+akravacyber@users.noreply.github.com>
2021-11-17 19:48:45 +0200

    Create dtap_full.py

---
+++ example_dags/dtap_full.py
@@ -0,0 +1,81 @@
+#
+# Licensed to the Apache Software Foundation (ASF) under one
+# or more contributor license agreements.  See the NOTICE file
+# distributed with this work for additional information
+# regarding copyright ownership.  The ASF licenses this file
+# to you under the Apache License, Version 2.0 (the
+# "License"); you may not use this file except in compliance
+# with the License.  You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing,
+# software distributed under the License is distributed on an
+# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+# KIND, either express or implied.  See the License for the
+# specific language governing permissions and limitations
+# under the License.
+
+from datetime import datetime, timedelta
+
+from airflow import DAG
+from airflow.operators.bash import BashOperator
+
+args = {
+    'owner': 'airflow',
+    'start_date': datetime.utcnow(),
+}
+
+dag = DAG(
+    dag_id='dtap_dag_with_bash',
+    default_args=args,
+    schedule_interval=timedelta(minutes=60),
+    tags=['example', 'dtap']
+)
+
+def ls(path: str):
+    return BashOperator(
+        task_id='ls_' + path + '_bash_op_dtap',
+        bash_command='hadoop fs -ls dtap://TenantStorage/' + path,
+        dag=dag
+    )
+
+def rm(path: str):
+    return BashOperator(
+        task_id='rm_' + path + '_bash_op_dtap',
+        bash_command='hadoop fs -rm dtap://TenantStorage/' + path,
+        dag=dag
+    )
+
+def cat(path: str):
+    return BashOperator(
+        task_id='cat_' + path + '_bash_op_dtap',
+        bash_command='hadoop fs -cat dtap://TenantStorage/' + path,
+        dag=dag
+    )
+
+def mkdir(path: str):
+    return BashOperator(
+        task_id='mkdir_' + path + '_bash_op_dtap',
+        bash_command='hadoop fs -mkdir dtap://TenantStorage/' + path,
+        dag=dag
+    )
+
+def put(path: str, data: str):
+    return BashOperator(
+        task_id='put_' + path + '_bash_op_dtap',
+        bash_command='echo "' + data + '" | hadoop fs -put - dtap://TenantStorage/' + path,
+        dag=dag
+    )
+
+def append(path: str, data: str): 
+    return BashOperator(
+        task_id='append_' + path + '_bash_op_dtap',
+        bash_command='echo "' + data + '" | hadoop fs -appendToFile - dtap://TenantStorage/' + path,
+        dag=dag
+    )
+
+ls('') >> mkdir('dtap_test_dir') >> put('dtap_test_dir/test.txt', 'Hello from DAG') >> cat('dtap_test_dir/test.txt') >> rm('dtap_test_dir')
+
+if __name__ == "__main__":
+    dag.cli()
