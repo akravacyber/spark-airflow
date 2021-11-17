2acf3478d6b0c9816262e83c08977665abcdaf71
Arkadii Kravchuk <64816141+akravacyber@users.noreply.github.com>
2021-11-17 21:51:12 +0200

    Update dtap_full.py

--- example_dags/dtap_full.py
+++ example_dags/dtap_full.py
@@ -79,7 +79,7 @@ def append(path: str, data: str):
 filename=str(random.randint(0, 99999999))
 data=str(datetime.utcnow())
 
-ls('') >> mkdir('dtap_test_dir') >> put('dtap_test_dir/test.txt', 'Hello from DAG') >> cat('dtap_test_dir/test.txt') >> rm('dtap_test_dir') >> append(filename, data)
+ls('') >> mkdir('dtap_test_dir') >> put('dtap_test_dir/test.txt', 'Hello from DAG' + str(random.randint(0, 9999))) >> cat('dtap_test_dir/test.txt') >> rm('dtap_test_dir') >> append(filename, data)
 
 
 if __name__ == "__main__":
