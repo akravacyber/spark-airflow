61fdddf96a2d3a674ba47ceb9d14a69a6f8ebcc0
Arkadii Kravchuk <64816141+akravacyber@users.noreply.github.com>
2023-09-28 12:46:44 +0000

    fix

--- example_dags/example_bash_operator_classic.py
+++ example_dags/example_bash_operator_classic.py
@@ -35,7 +35,13 @@ dag = DAG(
     default_args=args,
     schedule_interval='0 0 * * *',
     dagrun_timeout=timedelta(minutes=60),
-    tags=['example']
+    tags=['example'],
+    access_control={
+        'Segregated User': {
+            'can_read',
+            'can_edit'
+        }
+    }
 )
 
 run_this_last = DummyOperator(
