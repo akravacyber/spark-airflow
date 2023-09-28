5f6c2b560ff985b2f02a317d8745fea90a161a8a
Arkadii Kravchuk <64816141+akravacyber@users.noreply.github.com>
2023-09-28 12:24:33 +0000

    add spark example

---
+++ example_dags/example_spark_pi.py
@@ -0,0 +1,57 @@
+from airflow import DAG
+from airflow.models.param import Param
+from airflow.providers.cncf.kubernetes.operators.spark_kubernetes import SparkKubernetesOperator
+from airflow.providers.cncf.kubernetes.sensors.spark_kubernetes import SparkKubernetesSensor
+from airflow.utils.dates import days_ago
+
+default_args = {
+    'owner': 'airflow',
+    'depends_on_past': False,
+    'start_date': days_ago(1),
+    'email': ['airflow@example.com'],
+    'email_on_failure': False,
+    'email_on_retry': False,
+    'max_active_runs': 1,
+    'retries': 0
+}
+
+dag = DAG(
+    'spark_pi',
+    default_args=default_args,
+    schedule_interval=None,
+    tags=['ezaf', 'spark', 'pi'],
+    params={
+        'airgap_registry_url': Param("", type=["null", "string"], pattern=r"^$|^\S+/$")
+    },
+    render_template_as_native_obj=True,
+    access_control={
+        'role_hpedemo-user05': {
+            'can_read'
+        },
+        'role_hpedemo-user06': {
+            'can_read',
+            'can_edit'
+        }
+    }
+)
+
+submit = SparkKubernetesOperator(
+    task_id='submit',
+    namespace="spark",
+    application_file="example_spark_pi.yaml",
+    do_xcom_push=True,
+    dag=dag,
+    api_group="sparkoperator.hpe.com",
+    enable_impersonation_from_ldap_user=True
+)
+
+sensor = SparkKubernetesSensor(
+    task_id='monitor',
+    namespace="spark",
+    application_name="{{ task_instance.xcom_pull(task_ids='submit')['metadata']['name'] }}",
+    dag=dag,
+    api_group="sparkoperator.hpe.com",
+    attach_log=True
+)
+
+submit >> sensor
