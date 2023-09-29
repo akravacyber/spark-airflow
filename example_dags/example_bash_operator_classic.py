407808bae0387b4d5ccc42ce6042c343bf18ad6b
Arkadii Kravchuk <64816141+akravacyber@users.noreply.github.com>
2023-09-29 17:14:22 +0000

    test

--- example_dags/example_bash_operator_classic.py
+++ example_dags/example_bash_operator_classic.py
@@ -36,13 +36,13 @@ dag = DAG(
     schedule_interval='0 0 * * *',
     dagrun_timeout=timedelta(minutes=60),
     tags=['example'],
-    access_control={
-        'All': {
-            'can_read',
-            'can_edit',
-            'can_delete'
-        }
-    }
+    # access_control={
+    #     'All': {
+    #         'can_read',
+    #         'can_edit',
+    #         'can_delete'
+    #     }
+    # }
 )
 
 run_this_last = DummyOperator(
