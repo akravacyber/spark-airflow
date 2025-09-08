6e07aa1ca597e7d098ff9d777f89266d74b5b2d5
Arkadii Kravchuk <akravchuk@cybervisiontech.com>
2025-09-08 12:11:33 +0300

    test

---
+++ example_dags/test.py
@@ -0,0 +1,34 @@
+import logging
+
+from airflow import DAG
+from airflow.providers.standard.operators.bash import BashOperator
+from datetime import datetime
+
+log = logging.getLogger(__name__)
+
+
+default_args = {
+    'owner': 'airflow',
+    'start_date': datetime(2022, 1, 1)
+}
+
+with DAG(
+    dag_id='test_1_dag',
+    default_args=default_args,
+    schedule=None,
+    tags=['example'],
+) as dag:
+    
+    before = BashOperator(
+        task_id='before_1_dag',
+        bash_command='echo before triggering 1 && sleep 3',
+        dag=dag,
+    )
+
+    after = BashOperator(
+        task_id='after_1_dag',
+        bash_command='echo after triggering 1 && sleep 3',
+        dag=dag,
+    )
+
+    before >> after
